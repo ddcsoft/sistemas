ca2675a55ca5f516ae73b51d6acbd156a4c33e75
ddiazcor <ddiazcor@gmail.com>
2015-04-12 20:46:26 -0500

    volviendo a la version 2.7 de python. . .

--- sistemas/settings.py
+++ sistemas/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'sistemas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sistemas',
+        'USER': 'root',
+        'PASSWORD': 'orange2015',
+        'HOST': 'act.satranet.com',
+        'PORT': '3306',
     }
 }
 
